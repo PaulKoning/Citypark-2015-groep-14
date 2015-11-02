65e2d6c909c7b401113af66947c4ddbfd92a40d3
PaulKoning <paulkoning@live.nl>
2015-11-02 15:17:15 +0100

    Base
    Methode van type connection moet er zijn om het returntype connection
    moet zijn omdat conn een object van type Connection is. <<<<

--- Database.java
+++ Database.java
@@ -1,31 +1,27 @@
 import java.sql.*;
 import java.util.Properties; 
 
-class Database { 
-
+class Database{ 
 	private Connection conn;
-	public static void main(String[] args){
+	public Connection giveConnection(){
 		try{
 			//Get Connection
 			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/citypark", "root", "");
-			//Create a statement
-			/* Database test
-			/Statement Stat = conn.createStatement();
-			//Try statement
-			ResultSet res = Stat.executeQuery("Select * FROM gebruiker");
-			//Print result
-			while(res.next()){
-				System.out.println(res.getString("voornaam"));
-			}
-			*/
 		}
 		catch(Exception x)
 		{
 		x.printStackTrace();	
 		}
-	   
-	}
-	public Connection giveCon(){
 		return conn;
-	}
+}	
 }
+//Create a statement
+/* Database test
+/Statement Stat = conn.createStatement();
+//Try statement
+ResultSet res = Stat.executeQuery("Select * FROM gebruiker");
+//Print result
+while(res.next()){
+	System.out.println(res.getString("voornaam"));
+}
+*/
