8da867f54a5d3a721eee25d802a0aeba9ec12e72
Koen Berghuis <koenberghuis@gmail.com>
2015-11-02 15:05:27 +0100

    Commentaar toegevoegd

--- PROMAGReader/src/nl/hanze/promagreader/threading/In.java
+++ PROMAGReader/src/nl/hanze/promagreader/threading/In.java
@@ -2,40 +2,47 @@ package nl.hanze.promagreader.threading;
 
 import java.io.*;
 
+/*
+ * Thread om input te lezen
+ */
 public class In extends Thread {
-	private InputStream in;
-	private byte[] buffer;
-	private int max;
-	private String out;
-	private boolean blocked;
+	private InputStream in; //input
+	private byte[] buffer; //buffer voor het inlezen van de inputstream
+	private int max; //tot welke index de buffer is gevuld
+	private String out; //output
+	private boolean blocked; //mutex voor buffer
 	
 	public In(InputStream in) {
 		this.in=in;
-		buffer=new byte[50];
+		buffer=new byte[50]; 
 		max=0;
 		blocked=false;
 		start();
 	}
 	
+	/*
+	 * Geeft string representatie van de buffer
+	 * @returns {String} buffer
+	 */
 	protected String getBuffer() {
-		blocked=true;
+		blocked=true;	//blokkeer de buffer terwijl deze wordt gelezen
 		convertBufferToString();
-		blocked=false;
+		blocked=false; //geef de buffer vrij
 		return out;
 	}
-	
+
 	@Override
 	public void run() {
 		boolean skip=false;
 		while(true) {
 			try {
-				while(blocked) {}
-				int i=in.read();
-				skip=(i==-1);
+				while(blocked) {} //wacht tot de buffer is vrijgegeven
+				int i=in.read(); //lees de volgende byte van de inputstream
+				skip=(i==-1); //als de inputstream leeg is i niet aan de buffer toevoegen
 				if (!skip) {
-					byte b=(byte) i;
-					while(blocked) {}
-					buffer[max++]=b;
+					byte b=(byte) i; //int naar byte casten
+					while(blocked) {} //wacht tot de buffer is vrijgegeven
+					buffer[max++]=b; //nieuwe byte aan de eerste vrije plaats in de buffer toevoegen
 				}
 				skip=false;
 			} catch(Exception e) {
@@ -44,24 +51,29 @@ public class In extends Thread {
 		}
 	}
 	
+
+	/*
+	 * Converteert buffer naar String waarbij speciale bytes in juiste formaat worden gezet
+	 * @returns {String} buffer
+	 */
 	private void convertBufferToString() {
-		if (max==0) {out=null; return;}
+		if (max==0) {out=null; return;} //als de buffer leeg is null returnen
 		
-		StringBuffer stbTemp=new StringBuffer();
+		StringBuffer stbTemp=new StringBuffer(); //tijdelijke buffer om huidige buffer naar juiste formaat te converteren
 		
-		for(int i=0;i<max;i++) {
-			switch(buffer[i]) {
+		for(int i=0;i<max;i++) { //hele buffer lezen
+			switch(buffer[i]) { //speciale bytes naar ander formaat converteren
 				case 0x02 : stbTemp.append("STX "); break;
 				case 0x07 : stbTemp.append("BEL "); break;
-				case 0x0A : stbTemp.append("LF "); break;
+				case 0x0A : stbTemp.append("LF "); break; 
 				case 0x0D : stbTemp.append("CR "); break;
-				case 0x1B : stbTemp.append("ESC "); break;
+				case 0x1B : stbTemp.append("ESC "); break; 
 				default : stbTemp.append((char) buffer[i]);
 			} 
 		}
 		
-		stbTemp.append("\n\r");
-		max=0;
+		stbTemp.append("\n\r"); //
+		max=0; //buffer is gelezen en kan dus worden beschreven vanaf het begin
 		out=stbTemp.toString();
 	}
 	
