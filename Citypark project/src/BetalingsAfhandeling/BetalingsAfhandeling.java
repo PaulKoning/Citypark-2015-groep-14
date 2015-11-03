5ba42f918f0978c7f39e3ec416f31f3b9fa9ff13
PaulKoning <paulkoning@live.nl>
2015-11-03 19:05:15 +0100

    Work in progress

--- Citypark project/src/BetalingsAfhandeling/BetalingsAfhandeling.java
+++ Citypark project/src/BetalingsAfhandeling/BetalingsAfhandeling.java
@@ -2,11 +2,10 @@ package BetalingsAfhandeling;
 import Database.Database;
 
 import java.sql.*;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,6 +14,9 @@ public class BetalingsAfhandeling {
 	private Database connection;
 	public Connection conn;
 	private ArrayList<Map<String, Object>> list;
+	private Timestamp start;
+	private Timestamp finish;
+    private Calendar cal;
 	
 	public BetalingsAfhandeling(){
 		init();
@@ -46,46 +48,74 @@ public class BetalingsAfhandeling {
 	}
 	public void nextInc(Timestamp begin, Timestamp eind){
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		SimpleDateFormat day = new SimpleDateFormat("EEEEE");	
-		Timestamp start = begin;
-		Timestamp finish = eind;
-        Calendar cal = Calendar.getInstance();
+		start = begin;
+		finish = eind;
+        cal = Calendar.getInstance();
         cal.setTimeInMillis(start.getTime());
 
         System.out.println("Startijd begin "+start);        
         start = Timestamp.valueOf(sdf.format(cal.getTime()));
         
-        while(!(eind.before(start))){
+        while(!(finish.before(start))){
+        	
         	cal.add(Calendar.SECOND, 1800);
-            System.out.println(checkDay(cal.get(Calendar.DAY_OF_WEEK)));
         	start = Timestamp.valueOf(sdf.format(cal.getTime()));
         }
         System.out.println("Starttijd eind "+start);
-        System.out.println("Datbase eindtijd "+eind);
-        System.out.println(day.format(cal.get(Calendar.DAY_OF_WEEK)));
+        System.out.println("Datbase eindtijd "+finish);
+	}
 	
+	public HashMap<String, Double> getTarief(){
+		int zone = tariefZone(start, cal);
+		connection.query("Select tarief_id, bedragpuur, max FROM '"+zone+"' ");
+		list = connection.getResult();
+		HashMap<String, Double> tarief = new HashMap<String, Double>();
+		for(int i = 0; i < list.size(); i++) { 
+			tarief.put("tarief_id", (Double)list.get(i).get("tarief_id"));
+			tarief.put("bedragpuur", (Double)list.get(i).get("bedragpuur"));
+			tarief.put("max", (Double)list.get(i).get("max"));
+		}
+		return tarief;
+	}
+	public int tariefZone(Timestamp t, Calendar c){
+		Timestamp timeTemp = t;
+		Calendar calTemp = c;
+		DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+		Date date;
+		try {
+			date = (Date) dateFormat.parse("00:00:00");
+			long time = date.getTime();
+			new Timestamp(time);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(true){
+			
+		}
+		return 0;
 	}
 	public String checkDay(int a){
 		int dcount = a;
 		String day = null;
 		switch (dcount) {
-        case 1:  day = "Sun";
-                 break;
-        case 2:  day = "Mon";
-                 break;
-        case 3:  day = "Tue";
-                 break;
-        case 4:  day = "Wed";
-                 break;
-        case 5:  day = "Thu";
-                 break;
-        case 6:  day = "Fri";
-                 break;
-        case 7:  day = "Sat";
-                 break;
-        default: day = "Unknown";
-                 break;
-    }
+	        case 1:  day = "Sun";
+	                 break;
+	        case 2:  day = "Mon";
+	                 break;
+	        case 3:  day = "Tue";
+	                 break;
+	        case 4:  day = "Wed";
+	                 break;
+	        case 5:  day = "Thu";
+	                 break;
+	        case 6:  day = "Fri";
+	                 break;
+	        case 7:  day = "Sat";
+	                 break;
+	        default: day = "Unknown";
+	                 break;
+		}
 		return day;
 	}
 	public int checkTarief(int tijd){
