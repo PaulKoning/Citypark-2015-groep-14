51477d8b9e50e8ff7ad49f4eb3c2532eadfda69b
gerbranddegroot@hotmail.com <GerbrandDeGroot>
2015-11-05 13:22:19 +0100

    Project PROMAGReader toegevoegd en werkend in Citypark Project

--- Citypark project/src/main/Initialize.java
+++ Citypark project/src/main/Initialize.java
@@ -2,13 +2,18 @@ package main;
 import Database.Database;
 import scheduler.BetalingScheduler;
 import BetalingsAfhandeling.BetalingsAfhandeling;
+import nl.hanze.promagreader.main.Runner;
 
 public class Initialize {
 	public Initialize() {
+		String [] args = new String[2];
+	    args[0] = "hello";
+	    args[1] = "every";
 		try{
 		new BetalingsAfhandeling();
 		//new BetalingScheduler();
 		new Database();
+		Runner.main(args);
 		}
 		catch(Exception x){
 			x.printStackTrace();
