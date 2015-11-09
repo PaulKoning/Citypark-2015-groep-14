64bd05a0ea47c4a44a9bdde8583b7a239b3c0f1d
Koen Berghuis <koenberghuis@gmail.com>
2015-11-09 22:40:42 +0100

    weer server veranderd

--- Citypark project/src/bankclient/BankServiceLocator.java
+++ Citypark project/src/bankclient/BankServiceLocator.java
@@ -22,7 +22,7 @@ public class BankServiceLocator extends org.apache.axis.client.Service implement
     }
 
     // Use to get a proxy class for Bank
-    private java.lang.String Bank_address = "http://192.168.1.100:8080/Bank(1)/services/Bank";
+    private java.lang.String Bank_address = "localhost:8080/Bank/services/Bank";
 
     public java.lang.String getBankAddress() {
         return Bank_address;
